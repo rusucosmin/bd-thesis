7e43709d97fc9a32e9221ad2fe9eef29f5e10ecf
rusucosmin <cr.rusucosmin@gmail.com>
2018-04-01 21:20:28 +0300

    Added student2

--- research/experiment1/main.py
+++ research/experiment1/main.py
@@ -1,10 +1,12 @@
 from teacher import Teacher
 from student import Student
+from student2 import Student2
 from controller import Controller
 
 t = Teacher("teacher")
-students = [Student("student")]
+students = [Student2("student2"), Student("student")]
 c = Controller(t, students)
 
+#c.trainTeacher()
 c.trainStudents()
 
