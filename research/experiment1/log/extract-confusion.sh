c0123f448d5246e07438ab1f286d2629190a3dc6
rusucosmin <cr.rusucosmin@gmail.com>
2018-05-29 16:04:25 +0300

    Added extractor of confusion matrix

---
+++ research/experiment1/log/extract-confusion.sh
@@ -0,0 +1,216 @@
+echo "STUDENT"
+echo "T = 1"
+#awk 'FNR==2181 {printf "%s,\n", $0}' $1
+awk 'FNR>=2185 && FNR<=2199 {printf "%s,\n", $0}' $1
+
+echo "T = 3"
+#awk 'FNR==2249 {printf "%s,\n", $0}' $1
+awk 'FNR>=2253 && FNR<=2267 {printf "%s,\n", $0}' $1
+
+echo "T = 6"
+#awk 'FNR==2317 {printf "%s,\n", $0}' $1
+awk 'FNR>=2321 && FNR<=2335{printf "%s,\n", $0}' $1
+
+echo "T = 7"
+#awk 'FNR==2385 {printf "%s,\n", $0}' $1
+awk 'FNR>=2389 && FNR<=2403 {printf "%s,\n", $0}' $1
+
+echo "T = 8"
+#awk 'FNR==2453 {printf "%s,\n", $0}' $1
+awk 'FNR>=2457 && FNR<=2471 {printf "%s,\n", $0}' $1
+
+echo "T = 9"
+#awk 'FNR==2521 {printf "%s,\n", $0}' $1
+awk 'FNR>=2525 && FNR<=2539 {printf "%s,\n", $0}' $1
+
+echo "T = 10"
+#awk 'FNR==2589 {printf "%s,\n", $0}' $1
+awk 'FNR>=2593 && FNR<=2607 {printf "%s,\n", $0}' $1
+
+echo "T = 11"
+#awk 'FNR==2657 {printf "%s,\n", $0}' $1
+awk 'FNR>=2661 && FNR<=2675 {printf "%s,\n", $0}' $1
+
+echo "T = 12"
+#awk 'FNR==2725 {printf "%s,\n", $0}' $1
+awk 'FNR>=2729 && FNR<=2743 {printf "%s,\n", $0}' $1
+
+echo "T = 15"
+#awk 'FNR==2793 {printf "%s,\n", $0}' $1
+awk 'FNR>=2797 && FNR<=2811 {printf "%s,\n", $0}' $1
+
+echo "T = 20"
+#awk 'FNR==2861 {printf "%s,\n", $0}' $1
+awk 'FNR>=2865 && FNR<=2879 {printf "%s,\n", $0}' $1
+
+echo "STUDENT 2"
+echo "T = 1"
+#awk 'FNR==2929 {printf "%s,\n", $0}' $1
+awk 'FNR>=2933 && FNR<=2947 {printf "%s,\n", $0}' $1
+
+echo "T = 3"
+#awk 'FNR==2997 {printf "%s,\n", $0}' $1
+awk 'FNR>=3001 && FNR<=3015 {printf "%s,\n", $0}' $1
+
+echo "T = 6"
+#awk 'FNR==3065 {printf "%s,\n", $0}' $1
+awk 'FNR>=3069 && FNR<=3083 {printf "%s,\n", $0}' $1
+
+echo "T = 7"
+#awk 'FNR==3133 {printf "%s,\n", $0}' $1
+awk 'FNR>=3137 && FNR<=3151 {printf "%s,\n", $0}' $1
+
+echo "T = 8"
+#awk 'FNR==3201 {printf "%s,\n", $0}' $1
+awk 'FNR>=3205 && FNR<=3219 {printf "%s,\n", $0}' $1
+
+echo "T = 9"
+#awk 'FNR==3269 {printf "%s,\n", $0}' $1
+awk 'FNR>=3273 && FNR<=3287 {printf "%s,\n", $0}' $1
+
+echo "T = 10"
+#awk 'FNR==3337 {printf "%s,\n", $0}' $1
+awk 'FNR>=3341 && FNR<=3355{printf "%s,\n", $0}' $1
+
+echo "T = 11"
+#awk 'FNR==3405 {printf "%s,\n", $0}' $1
+awk 'FNR>=3409 && FNR<=3423 {printf "%s,\n", $0}' $1
+
+echo "T = 12"
+#awk 'FNR==3473 {printf "%s,\n", $0}' $1
+awk 'FNR>=3477 && FNR<=3491 {printf "%s,\n", $0}' $1
+
+echo "T = 15"
+#awk 'FNR==3541 {printf "%s,\n", $0}' $1
+awk 'FNR>=3545 && FNR<=3559 {printf "%s,\n", $0}' $1
+
+echo "T = 20"
+#awk 'FNR==3609 {printf "%s,\n", $0}' $1
+awk 'FNR>=3613 && FNR<=3627 {printf "%s,\n", $0}' $1
+
+echo "STUDENT 3"
+echo "T = 1"
+#awk 'FNR==3677 {printf "%s,\n", $0}' $1
+awk 'FNR>=3681 && FNR<=3695 {printf "%s,\n", $0}' $1
+
+echo "T = 3"
+#awk 'FNR==3745 {printf "%s,\n", $0}' $1
+awk 'FNR>=3749 && FNR<=3763 {printf "%s,\n", $0}' $1
+
+echo "T = 6"
+#awk 'FNR==3813 {printf "%s,\n", $0}' $1
+awk 'FNR>=3817 && FNR<=3831 {printf "%s,\n", $0}' $1
+
+echo "T = 7"
+#awk 'FNR==3881 {printf "%s,\n", $0}' $1
+awk 'FNR>=3885 && FNR<=3899 {printf "%s,\n", $0}' $1
+
+echo "T = 8"
+#awk 'FNR==3949 {printf "%s,\n", $0}' $1
+awk 'FNR>=3953 && FNR<=3967 {printf "%s,\n", $0}' $1
+
+echo "T = 9"
+#awk 'FNR==4017{printf "%s,\n", $0}' $1
+awk 'FNR>=4021&& FNR<=4035{printf "%s,\n", $0}' $1
+
+echo "T = 10"
+#awk 'FNR==4085{printf "%s,\n", $0}' $1
+awk 'FNR>=4089&& FNR<=4103{printf "%s,\n", $0}' $1
+
+echo "T = 11"
+#awk 'FNR==4153{printf "%s,\n", $0}' $1
+awk 'FNR>=4157&& FNR<=4171{printf "%s,\n", $0}' $1
+
+echo "T = 12"
+#awk 'FNR==4221{printf "%s,\n", $0}' $1
+awk 'FNR>=4225&& FNR<=4239{printf "%s,\n", $0}' $1
+
+echo "T = 15"
+#awk 'FNR==4289{printf "%s,\n", $0}' $1
+awk 'FNR>=4293&& FNR<=4307{printf "%s,\n", $0}' $1
+
+echo "T = 20"
+#awk 'FNR==4357{printf "%s,\n", $0}' $1
+awk 'FNR>=4361&& FNR<=4375{printf "%s,\n", $0}' $1
+
+echo "STUDENT 4"
+echo "T = 1"
+#awk 'FNR==685 {printf "%s,\n", $0}' $1
+awk 'FNR>=690 && FNR<=703 {printf "%s,\n", $0}' $1
+echo "T = 3"
+#awk 'FNR==753 {printf "%s,\n", $0}' $1
+awk 'FNR>=757 && FNR<=771 {printf "%s,\n", $0}' $1
+echo "T = 6"
+#awk 'FNR==821 {printf "%s,\n", $0}' $1
+awk 'FNR>=825 && FNR<=839 {printf "%s,\n", $0}' $1
+echo "T = 7"
+#awk 'FNR==889 {printf "%s,\n", $0}' $1
+awk 'FNR>=893 && FNR<=907 {printf "%s,\n", $0}' $1
+echo "T = 8"
+#awk 'FNR==957 {printf "%s,\n", $0}' $1
+awk 'FNR>=961 && FNR<=975 {printf "%s,\n", $0}' $1
+echo "T = 9"
+#awk 'FNR==1025 {printf "%s,\n", $0}' $1
+awk 'FNR>=1029 && FNR<=1043 {printf "%s,\n", $0}' $1
+echo "T = 10"
+#awk 'FNR==1093 {printf "%s,\n", $0}' $1
+awk 'FNR>=1097 && FNR<=1111 {printf "%s,\n", $0}' $1
+echo "T = 11"
+#awk 'FNR==1161 {printf "%s,\n", $0}' $1
+awk 'FNR>=1165 && FNR<=1179 {printf "%s,\n", $0}' $1
+echo "T = 12"
+#awk 'FNR==1229 {printf "%s,\n", $0}' $1
+awk 'FNR>=1233 && FNR<=1247 {printf "%s,\n", $0}' $1
+echo "T = 15"
+#awk 'FNR==1297 {printf "%s,\n", $0}' $1
+awk 'FNR>=1301 && FNR<=1315 {printf "%s,\n", $0}' $1
+echo "T = 20"
+#awk 'FNR==1365 {printf "%s,\n", $0}' $1
+awk 'FNR>=1369 && FNR<=1383 {printf "%s,\n", $0}' $1
+
+echo "STUDENT 5"
+echo "T = 1"
+#awk 'FNR==1433 {printf "%s,\n", $0}' $1
+awk 'FNR>=1437 && FNR<=1451 {printf "%s,\n", $0}' $1
+
+echo "T = 3"
+#awk 'FNR==1501 {printf "%s,\n", $0}' $1
+awk 'FNR>=1505 && FNR<=1519 {printf "%s,\n", $0}' $1
+
+echo "T = 6"
+#awk 'FNR==1569 {printf "%s,\n", $0}' $1
+awk 'FNR>=1573 && FNR<=1587 {printf "%s,\n", $0}' $1
+
+echo "T = 7"
+#awk 'FNR==1637 {printf "%s,\n", $0}' $1
+awk 'FNR>=1641 && FNR<=1655 {printf "%s,\n", $0}' $1
+
+echo "T = 8"
+#awk 'FNR==1705 {printf "%s,\n", $0}' $1
+awk 'FNR>=1709 && FNR<=1723 {printf "%s,\n", $0}' $1
+
+echo "T = 9"
+#awk 'FNR==1773 {printf "%s,\n", $0}' $1
+awk 'FNR>=1777 && FNR<=1791 {printf "%s,\n", $0}' $1
+
+echo "T = 10"
+#awk 'FNR==1841 {printf "%s,\n", $0}' $1
+awk 'FNR>=1845 && FNR<=1859 {printf "%s,\n", $0}' $1
+
+echo "T = 11"
+#awk 'FNR==1909 {printf "%s,\n", $0}' $1
+awk 'FNR>=1913 && FNR<=1927 {printf "%s,\n", $0}' $1
+
+echo "T = 12"
+#awk 'FNR==1977 {printf "%s,\n", $0}' $1
+awk 'FNR>=1981 && FNR<=1995 {printf "%s,\n", $0}' $1
+
+echo "T = 15"
+#awk 'FNR==2045 {printf "%s,\n", $0}' $1
+awk 'FNR>=2049 && FNR<= 2063 {printf "%s,\n", $0}' $1
+
+echo "T = 20"
+#awk 'FNR==2113 {printf "%s,\n", $0}' $1
+awk 'FNR>=2117 && FNR<=2131 {printf "%s,\n", $0}' $1
+
+
