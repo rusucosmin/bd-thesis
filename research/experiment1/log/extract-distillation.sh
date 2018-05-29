6336cb3262ea978bc8a4bd638e185026bd625d7b
rusucosmin <cr.rusucosmin@gmail.com>
2018-05-29 15:32:44 +0300

    Removed old logs

--- research/experiment1/log/extract-distillation.sh
+++ research/experiment1/log/extract-distillation.sh
@@ -1,216 +1,216 @@
 echo "STUDENT"
 echo "T = 1"
-awk 'FNR==2181 {print FILENAME, $0}' $1
-#awk 'FNR>=2185 && FNR<=2199 {print FILENAME, $0}' $1
+#awk 'FNR==2181 {print FILENAME, $0}' $1
+awk 'FNR>=2185 && FNR<=2199 {print FILENAME, $0}' $1
 
 echo "T = 3"
-awk 'FNR==2249 {print FILENAME, $0}' $1
-#awk 'FNR>=2253 && FNR<=2267 {print FILENAME, $0}' $1
+#awk 'FNR==2249 {print FILENAME, $0}' $1
+awk 'FNR>=2253 && FNR<=2267 {print FILENAME, $0}' $1
 
 echo "T = 6"
-awk 'FNR==2317 {print FILENAME, $0}' $1
-#awk 'FNR>=2321 && FNR<=2335{print FILENAME, $0}' $1
+#awk 'FNR==2317 {print FILENAME, $0}' $1
+awk 'FNR>=2321 && FNR<=2335{print FILENAME, $0}' $1
 
 echo "T = 7"
-awk 'FNR==2385 {print FILENAME, $0}' $1
-#awk 'FNR>=2389 && FNR<=2403 {print FILENAME, $0}' $1
+#awk 'FNR==2385 {print FILENAME, $0}' $1
+awk 'FNR>=2389 && FNR<=2403 {print FILENAME, $0}' $1
 
 echo "T = 8"
-awk 'FNR==2453 {print FILENAME, $0}' $1
-#awk 'FNR>=2457 && FNR<=2471 {print FILENAME, $0}' $1
+#awk 'FNR==2453 {print FILENAME, $0}' $1
+awk 'FNR>=2457 && FNR<=2471 {print FILENAME, $0}' $1
 
 echo "T = 9"
-awk 'FNR==2521 {print FILENAME, $0}' $1
-#awk 'FNR>=2525 && FNR<=2539 {print FILENAME, $0}' $1
+#awk 'FNR==2521 {print FILENAME, $0}' $1
+awk 'FNR>=2525 && FNR<=2539 {print FILENAME, $0}' $1
 
 echo "T = 10"
-awk 'FNR==2589 {print FILENAME, $0}' $1
-#awk 'FNR>=2593 && FNR<=2607 {print FILENAME, $0}' $1
+#awk 'FNR==2589 {print FILENAME, $0}' $1
+awk 'FNR>=2593 && FNR<=2607 {print FILENAME, $0}' $1
 
 echo "T = 11"
-awk 'FNR==2657 {print FILENAME, $0}' $1
-#awk 'FNR>=2661 && FNR<=2675 {print FILENAME, $0}' $1
+#awk 'FNR==2657 {print FILENAME, $0}' $1
+awk 'FNR>=2661 && FNR<=2675 {print FILENAME, $0}' $1
 
 echo "T = 12"
-awk 'FNR==2725 {print FILENAME, $0}' $1
-#awk 'FNR>=2729 && FNR<=2743 {print FILENAME, $0}' $1
+#awk 'FNR==2725 {print FILENAME, $0}' $1
+awk 'FNR>=2729 && FNR<=2743 {print FILENAME, $0}' $1
 
 echo "T = 15"
-awk 'FNR==2793 {print FILENAME, $0}' $1
-#awk 'FNR>=2797 && FNR<=2811 {print FILENAME, $0}' $1
+#awk 'FNR==2793 {print FILENAME, $0}' $1
+awk 'FNR>=2797 && FNR<=2811 {print FILENAME, $0}' $1
 
 echo "T = 20"
-awk 'FNR==2861 {print FILENAME, $0}' $1
-#awk 'FNR>=2865 && FNR<=2879 {print FILENAME, $0}' $1
+#awk 'FNR==2861 {print FILENAME, $0}' $1
+awk 'FNR>=2865 && FNR<=2879 {print FILENAME, $0}' $1
 
 echo "STUDENT 2"
 echo "T = 1"
-awk 'FNR==2929 {print FILENAME, $0}' $1
-#awk 'FNR>=2933 && FNR<=2947 {print FILENAME, $0}' $1
+#awk 'FNR==2929 {print FILENAME, $0}' $1
+awk 'FNR>=2933 && FNR<=2947 {print FILENAME, $0}' $1
 
 echo "T = 3"
-awk 'FNR==2997 {print FILENAME, $0}' $1
-#awk 'FNR>=3001 && FNR<=3015 {print FILENAME, $0}' $1
+#awk 'FNR==2997 {print FILENAME, $0}' $1
+awk 'FNR>=3001 && FNR<=3015 {print FILENAME, $0}' $1
 
 echo "T = 6"
-awk 'FNR==3065 {print FILENAME, $0}' $1
-#awk 'FNR>=3069 && FNR<=3083 {print FILENAME, $0}' $1
+#awk 'FNR==3065 {print FILENAME, $0}' $1
+awk 'FNR>=3069 && FNR<=3083 {print FILENAME, $0}' $1
 
 echo "T = 7"
-awk 'FNR==3133 {print FILENAME, $0}' $1
-#awk 'FNR>=3137 && FNR<=3151 {print FILENAME, $0}' $1
+#awk 'FNR==3133 {print FILENAME, $0}' $1
+awk 'FNR>=3137 && FNR<=3151 {print FILENAME, $0}' $1
 
 echo "T = 8"
-awk 'FNR==3201 {print FILENAME, $0}' $1
-#awk 'FNR>=3205 && FNR<=3219 {print FILENAME, $0}' $1
+#awk 'FNR==3201 {print FILENAME, $0}' $1
+awk 'FNR>=3205 && FNR<=3219 {print FILENAME, $0}' $1
 
 echo "T = 9"
-awk 'FNR==3269 {print FILENAME, $0}' $1
-#awk 'FNR>=3273 && FNR<=3287 {print FILENAME, $0}' $1
+#awk 'FNR==3269 {print FILENAME, $0}' $1
+awk 'FNR>=3273 && FNR<=3287 {print FILENAME, $0}' $1
 
 echo "T = 10"
-awk 'FNR==3337 {print FILENAME, $0}' $1
-#awk 'FNR>=3341 && FNR<=3355{print FILENAME, $0}' $1
+#awk 'FNR==3337 {print FILENAME, $0}' $1
+awk 'FNR>=3341 && FNR<=3355{print FILENAME, $0}' $1
 
 echo "T = 11"
-awk 'FNR==3405 {print FILENAME, $0}' $1
-#awk 'FNR>=3409 && FNR<=3423 {print FILENAME, $0}' $1
+#awk 'FNR==3405 {print FILENAME, $0}' $1
+awk 'FNR>=3409 && FNR<=3423 {print FILENAME, $0}' $1
 
 echo "T = 12"
-awk 'FNR==3473 {print FILENAME, $0}' $1
-#awk 'FNR>=3477 && FNR<=3491 {print FILENAME, $0}' $1
+#awk 'FNR==3473 {print FILENAME, $0}' $1
+awk 'FNR>=3477 && FNR<=3491 {print FILENAME, $0}' $1
 
 echo "T = 15"
-awk 'FNR==3541 {print FILENAME, $0}' $1
-#awk 'FNR>=3545 && FNR<=3559 {print FILENAME, $0}' $1
+#awk 'FNR==3541 {print FILENAME, $0}' $1
+awk 'FNR>=3545 && FNR<=3559 {print FILENAME, $0}' $1
 
 echo "T = 20"
-awk 'FNR==3609 {print FILENAME, $0}' $1
-#awk 'FNR>=3613 && FNR<=3627 {print FILENAME, $0}' $1
+#awk 'FNR==3609 {print FILENAME, $0}' $1
+awk 'FNR>=3613 && FNR<=3627 {print FILENAME, $0}' $1
 
 echo "STUDENT 3"
 echo "T = 1"
-awk 'FNR==3677 {print FILENAME, $0}' $1
-#awk 'FNR>=3681 && FNR<=3695 {print FILENAME, $0}' $1
+#awk 'FNR==3677 {print FILENAME, $0}' $1
+awk 'FNR>=3681 && FNR<=3695 {print FILENAME, $0}' $1
 
 echo "T = 3"
-awk 'FNR==3745 {print FILENAME, $0}' $1
-#awk 'FNR>=3749 && FNR<=3763 {print FILENAME, $0}' $1
+#awk 'FNR==3745 {print FILENAME, $0}' $1
+awk 'FNR>=3749 && FNR<=3763 {print FILENAME, $0}' $1
 
 echo "T = 6"
-awk 'FNR==3813 {print FILENAME, $0}' $1
-#awk 'FNR>=3817 && FNR<=3831 {print FILENAME, $0}' $1
+#awk 'FNR==3813 {print FILENAME, $0}' $1
+awk 'FNR>=3817 && FNR<=3831 {print FILENAME, $0}' $1
 
 echo "T = 7"
-awk 'FNR==3881 {print FILENAME, $0}' $1
-#awk 'FNR>=3885 && FNR<=3899 {print FILENAME, $0}' $1
+#awk 'FNR==3881 {print FILENAME, $0}' $1
+awk 'FNR>=3885 && FNR<=3899 {print FILENAME, $0}' $1
 
 echo "T = 8"
-awk 'FNR==3949 {print FILENAME, $0}' $1
-#awk 'FNR>=3953 && FNR<=3967 {print FILENAME, $0}' $1
+#awk 'FNR==3949 {print FILENAME, $0}' $1
+awk 'FNR>=3953 && FNR<=3967 {print FILENAME, $0}' $1
 
 echo "T = 9"
-awk 'FNR==4017{print FILENAME, $0}' $1
-#awk 'FNR>=4021&& FNR<=4035{print FILENAME, $0}' $1
+#awk 'FNR==4017{print FILENAME, $0}' $1
+awk 'FNR>=4021&& FNR<=4035{print FILENAME, $0}' $1
 
 echo "T = 10"
-awk 'FNR==4085{print FILENAME, $0}' $1
-#awk 'FNR>=4089&& FNR<=4103{print FILENAME, $0}' $1
+#awk 'FNR==4085{print FILENAME, $0}' $1
+awk 'FNR>=4089&& FNR<=4103{print FILENAME, $0}' $1
 
 echo "T = 11"
-awk 'FNR==4153{print FILENAME, $0}' $1
-#awk 'FNR>=4157&& FNR<=4171{print FILENAME, $0}' $1
+#awk 'FNR==4153{print FILENAME, $0}' $1
+awk 'FNR>=4157&& FNR<=4171{print FILENAME, $0}' $1
 
 echo "T = 12"
-awk 'FNR==4221{print FILENAME, $0}' $1
-#awk 'FNR>=4225&& FNR<=4239{print FILENAME, $0}' $1
+#awk 'FNR==4221{print FILENAME, $0}' $1
+awk 'FNR>=4225&& FNR<=4239{print FILENAME, $0}' $1
 
 echo "T = 15"
-awk 'FNR==4289{print FILENAME, $0}' $1
-#awk 'FNR>=4293&& FNR<=4307{print FILENAME, $0}' $1
+#awk 'FNR==4289{print FILENAME, $0}' $1
+awk 'FNR>=4293&& FNR<=4307{print FILENAME, $0}' $1
 
 echo "T = 20"
-awk 'FNR==4357{print FILENAME, $0}' $1
-#awk 'FNR>=4361&& FNR<=4375{print FILENAME, $0}' $1
+#awk 'FNR==4357{print FILENAME, $0}' $1
+awk 'FNR>=4361&& FNR<=4375{print FILENAME, $0}' $1
 
 echo "STUDENT 4"
 echo "T = 1"
-awk 'FNR==685 {print FILENAME, $0}' $1
-#awk 'FNR>=690 && FNR<=703 {print FILENAME, $0}' $1
+#awk 'FNR==685 {print FILENAME, $0}' $1
+awk 'FNR>=690 && FNR<=703 {print FILENAME, $0}' $1
 echo "T = 3"
-awk 'FNR==753 {print FILENAME, $0}' $1
-#awk 'FNR>=757 && FNR<=771 {print FILENAME, $0}' $1
+#awk 'FNR==753 {print FILENAME, $0}' $1
+awk 'FNR>=757 && FNR<=771 {print FILENAME, $0}' $1
 echo "T = 6"
-awk 'FNR==821 {print FILENAME, $0}' $1
-#awk 'FNR>=825 && FNR<=839 {print FILENAME, $0}' $1
+#awk 'FNR==821 {print FILENAME, $0}' $1
+awk 'FNR>=825 && FNR<=839 {print FILENAME, $0}' $1
 echo "T = 7"
-awk 'FNR==889 {print FILENAME, $0}' $1
-#awk 'FNR>=893 && FNR<=907 {print FILENAME, $0}' $1
+#awk 'FNR==889 {print FILENAME, $0}' $1
+awk 'FNR>=893 && FNR<=907 {print FILENAME, $0}' $1
 echo "T = 8"
-awk 'FNR==957 {print FILENAME, $0}' $1
-#awk 'FNR>=961 && FNR<=975 {print FILENAME, $0}' $1
+#awk 'FNR==957 {print FILENAME, $0}' $1
+awk 'FNR>=961 && FNR<=975 {print FILENAME, $0}' $1
 echo "T = 9"
-awk 'FNR==1025 {print FILENAME, $0}' $1
-#awk 'FNR>=1029 && FNR<=1043 {print FILENAME, $0}' $1
+#awk 'FNR==1025 {print FILENAME, $0}' $1
+awk 'FNR>=1029 && FNR<=1043 {print FILENAME, $0}' $1
 echo "T = 10"
-awk 'FNR==1093 {print FILENAME, $0}' $1
-#awk 'FNR>=1097 && FNR<=1111 {print FILENAME, $0}' $1
+#awk 'FNR==1093 {print FILENAME, $0}' $1
+awk 'FNR>=1097 && FNR<=1111 {print FILENAME, $0}' $1
 echo "T = 11"
-awk 'FNR==1161 {print FILENAME, $0}' $1
-#awk 'FNR>=1165 && FNR<=1179 {print FILENAME, $0}' $1
+#awk 'FNR==1161 {print FILENAME, $0}' $1
+awk 'FNR>=1165 && FNR<=1179 {print FILENAME, $0}' $1
 echo "T = 12"
-awk 'FNR==1229 {print FILENAME, $0}' $1
-#awk 'FNR>=1233 && FNR<=1247 {print FILENAME, $0}' $1
+#awk 'FNR==1229 {print FILENAME, $0}' $1
+awk 'FNR>=1233 && FNR<=1247 {print FILENAME, $0}' $1
 echo "T = 15"
-awk 'FNR==1297 {print FILENAME, $0}' $1
-#awk 'FNR>=1301 && FNR<=1315 {print FILENAME, $0}' $1
+#awk 'FNR==1297 {print FILENAME, $0}' $1
+awk 'FNR>=1301 && FNR<=1315 {print FILENAME, $0}' $1
 echo "T = 20"
-awk 'FNR==1365 {print FILENAME, $0}' $1
-#awk 'FNR>=1369 && FNR<=1383 {print FILENAME, $0}' $1
+#awk 'FNR==1365 {print FILENAME, $0}' $1
+awk 'FNR>=1369 && FNR<=1383 {print FILENAME, $0}' $1
 
 echo "STUDENT 5"
 echo "T = 1"
-awk 'FNR==1433 {print FILENAME, $0}' $1
-#awk 'FNR>=1437 && FNR<=1451 {print FILENAME, $0}' $1
+#awk 'FNR==1433 {print FILENAME, $0}' $1
+awk 'FNR>=1437 && FNR<=1451 {print FILENAME, $0}' $1
 
 echo "T = 3"
-awk 'FNR==1501 {print FILENAME, $0}' $1
-#awk 'FNR>=1505 && FNR<=1519 {print FILENAME, $0}' $1
+#awk 'FNR==1501 {print FILENAME, $0}' $1
+awk 'FNR>=1505 && FNR<=1519 {print FILENAME, $0}' $1
 
 echo "T = 6"
-awk 'FNR==1569 {print FILENAME, $0}' $1
-#awk 'FNR>=1573 && FNR<=1587 {print FILENAME, $0}' $1
+#awk 'FNR==1569 {print FILENAME, $0}' $1
+awk 'FNR>=1573 && FNR<=1587 {print FILENAME, $0}' $1
 
 echo "T = 7"
-awk 'FNR==1637 {print FILENAME, $0}' $1
-#awk 'FNR>=1641 && FNR<=1655 {print FILENAME, $0}' $1
+#awk 'FNR==1637 {print FILENAME, $0}' $1
+awk 'FNR>=1641 && FNR<=1655 {print FILENAME, $0}' $1
 
 echo "T = 8"
-awk 'FNR==1705 {print FILENAME, $0}' $1
-#awk 'FNR>=1709 && FNR<=1723 {print FILENAME, $0}' $1
+#awk 'FNR==1705 {print FILENAME, $0}' $1
+awk 'FNR>=1709 && FNR<=1723 {print FILENAME, $0}' $1
 
 echo "T = 9"
-awk 'FNR==1773 {print FILENAME, $0}' $1
-#awk 'FNR>=1777 && FNR<=1791 {print FILENAME, $0}' $1
+#awk 'FNR==1773 {print FILENAME, $0}' $1
+awk 'FNR>=1777 && FNR<=1791 {print FILENAME, $0}' $1
 
 echo "T = 10"
-awk 'FNR==1841 {print FILENAME, $0}' $1
-#awk 'FNR>=1845 && FNR<=1859 {print FILENAME, $0}' $1
+#awk 'FNR==1841 {print FILENAME, $0}' $1
+awk 'FNR>=1845 && FNR<=1859 {print FILENAME, $0}' $1
 
 echo "T = 11"
-awk 'FNR==1909 {print FILENAME, $0}' $1
-#awk 'FNR>=1913 && FNR<=1927 {print FILENAME, $0}' $1
+#awk 'FNR==1909 {print FILENAME, $0}' $1
+awk 'FNR>=1913 && FNR<=1927 {print FILENAME, $0}' $1
 
 echo "T = 12"
-awk 'FNR==1977 {print FILENAME, $0}' $1
-#awk 'FNR>=1981 && FNR<=1995 {print FILENAME, $0}' $1
+#awk 'FNR==1977 {print FILENAME, $0}' $1
+awk 'FNR>=1981 && FNR<=1995 {print FILENAME, $0}' $1
 
 echo "T = 15"
-awk 'FNR==2045 {print FILENAME, $0}' $1
-#awk 'FNR>=2049 && FNR<= 2063 {print FILENAME, $0}' $1
+#awk 'FNR==2045 {print FILENAME, $0}' $1
+awk 'FNR>=2049 && FNR<= 2063 {print FILENAME, $0}' $1
 
 echo "T = 20"
-awk 'FNR==2113 {print FILENAME, $0}' $1
-#awk 'FNR>=2117 && FNR<=2131 {print FILENAME, $0}' $1
+#awk 'FNR==2113 {print FILENAME, $0}' $1
+awk 'FNR>=2117 && FNR<=2131 {print FILENAME, $0}' $1
 
 
